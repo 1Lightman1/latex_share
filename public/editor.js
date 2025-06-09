c27eec04d0699e3c3c0c539d10c309d0b861cef2
1Lightman1 <enzoandreani7@gmail.com>
2025-06-09 13:02:27 +0200

    j

--- public/editor.js
+++ public/editor.js
@@ -1,12 +1,31 @@
 const socket = io();
 const textarea = document.getElementById("editor");
+const preview = document.getElementById("preview");
+
+function updatePreview() {
+  axios.post('/compile', { latex: textarea.value })
+    .then(res => {
+      preview.src = '/output/output.pdf?t=' + new Date().getTime();
+    }).catch(err => console.error(err));
+}
 
 textarea.addEventListener("input", () => {
   socket.emit("text-update", textarea.value);
+  updatePreview();
 });
 
 socket.on("text-update", (newText) => {
   if (textarea.value !== newText) {
     textarea.value = newText;
+    updatePreview();
   }
 });
+
+function downloadPDF() {
+  const link = document.createElement('a');
+  link.href = '/output/output.pdf';
+  link.download = 'document.pdf';
+  link.click();
+}
+
+window.onload = updatePreview;
--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const { exec } = require('child_process');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-let sharedText = "";
+let sharedText = '';
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/output', express.static(path.join(__dirname, 'output')));
+app.use(bodyParser.json());
 
 io.on('connection', (socket) => {
   socket.emit('text-update', sharedText);
-
   socket.on('text-update', (newText) => {
     sharedText = newText;
     socket.broadcast.emit('text-update', newText);
+  });
+});
+
+app.post('/compile', (req, res) => {
+  const latexContent = req.body.latex;
+  const texFile = path.join(__dirname, 'output', 'output.tex');
+  const outputDir = path.join(__dirname, 'output');
+
+  if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+
+  fs.writeFileSync(texFile, latexContent);
+
+  exec(`pdflatex -output-directory=${outputDir} ${texFile}`, (err) => {
+    if (err) return res.status(500).send('Erreur de compilation LaTeX');
+    res.send('OK');
   });
 });
 
